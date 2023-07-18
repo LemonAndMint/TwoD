6ead616a06d34eb94c38fbdec3cd2c56f0149aa3
LemonAndMint <orn.betul@gmail.com>
2023-07-18 12:43:06 +0300

    changed private variable set phase

---
+++ Assets/Scripts/Bullet/BulletStats.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletStats : MonoBehaviour
+{
+    public float damageValue;
+    public float bulletSpeed;
+
+    /*
+     * Property init alternatif cozum. Init setter unity'nin desteklemedigi bir .net 5 ozelligi. 
+     * Bunun icin sinif icerisinde sadece Instatintiate metodunda deger atanir. \ Corpyr. 
+     */
+
+    private int _targetEnemyID; 
+    public int targetEnemyID{ 
+
+        get { 
+
+            return _targetEnemyID;
+
+        }
+        
+        private set{} 
+        
+    }
+    
+    private Transform _targetTransform;
+    public Transform targetTransform { 
+        
+        get { 
+
+            return _targetTransform;
+
+        }
+        
+        private set{} 
+        
+    }
+
+    public void SetBulletStats(float damageValue, Transform targetTransform, float bulletSpeed = 4f){
+
+        this.damageValue = damageValue;
+        this._targetTransform = targetTransform;
+        this.bulletSpeed = bulletSpeed;
+
+        _targetEnemyID = targetTransform.GetComponent<EnemyStats>().enemyInGameID; 
+
+    }
+
+    public void DestroyBullet(){ //#FIXME? baska bir sinifta yazilabilir mi? \ Corpyr.
+
+        Destroy(this.gameObject);
+
+    }
+}
--- Assets/Scripts/Enemy/EnemyStats.cs
+++ Assets/Scripts/Enemy/EnemyStats.cs
@@ -4,9 +4,32 @@ using UnityEngine;
 
 public class EnemyStats : MonoBehaviour
 {
-    private int enemyInGameID;
     public float enemyHealth;
-
     public float enemyMoveSpeed;
+
+    private int _enemyInGameID;
+    public int enemyInGameID 
+    { 
+        get{ return _enemyInGameID; }
+
+        private set{ }
+
+    }
+
+    public void SetEnemyStats(float enemyHealth, float enemyMoveSpeed, int enemyInGameID){
+
+        this.enemyHealth = enemyHealth;
+        this.enemyMoveSpeed = enemyMoveSpeed;
+
+        this._enemyInGameID = enemyInGameID;
+
+    }    
+
+    public void SetEnemyStats(int enemyInGameID){
+
+        this._enemyInGameID = enemyInGameID;
+
+    }    
+
 
 }
